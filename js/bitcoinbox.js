d4b228e2656617993af7c7494257c37da053d33d
Jan Vornberger <jan@uos.de>
2015-03-06 21:50:47 +0100

    Fetch project status via Firebase

---
+++ js/bitcoinbox.js
@@ -0,0 +1,19 @@
+var FIREBASE_URL = 'https://bitcoinbox.firebaseio.com/';
+var EXCHANGE_RATE = 265;
+
+$(document).ready(function () {
+    var db = new Firebase(FIREBASE_URL);
+
+    db.on('value', function(snapshot) {
+        percentage = snapshot.val().percentage;
+        pledged = snapshot.val().pledged;
+        pledged_usd = Math.round(pledged * EXCHANGE_RATE);
+        backers = snapshot.val().backers;
+        status_desc =  pledged + " BTC / " + pledged_usd + " USD; " +
+                        backers + " backer";
+        if (backers != 1) status_desc += "s";
+
+        $('#status-percentage').width(percentage + '%');
+        $('#status-desc').text(status_desc);
+    });
+})
